fd3b4f2603a8487e900db574c7063629d4da9592
Jwarfield815 <creepathan@gmail.com>
2020-02-04 20:15:53 -0600

    completed exercise 2-1

--- exercise_2_1/display_discount.php
+++ exercise_2_1/display_discount.php
@@ -1,33 +1,42 @@
 <?php
     $product_description = filter_input(INPUT_POST, 'product_description');
-    $list_price = filter_input(INPUT_POST, 'list_price', FILTER_VALIDATE_FLOAT);
-    $discount_percent = filter_input(INPUT_POST, 'discount_percent', FILTER_VALIDATE_INT);
+    $list_price = filter_input(INPUT_POST, 'list_price');
+    $discount_percent = filter_input(INPUT_POST, 'discount_percent');
+
+        $discount = $list_price * $discount_percent * 0.01;
+        $discount_price = $list_price - $discount;
+
+        $format_list_price = '$'.number_format($list_price, 2);
+        $format_discount_percent = $discount_percent.'%';
+        $format_discount = '$'.number_format($discount, 2);
+        $format_discount_price = '$'.number_format($discount_price, 2);
 ?>
 
 <!DOCTYPE html>
 <html>
-<head>
-    <title>Product Discount Calculator</title>
-    <link rel="stylesheet" type="text/css" href="main.css">
-</head>
-<body>
-    <main>
-        <h1>This page is under construction</h1>
+    <head>
+        <title>Product Discount Calculator</title>
+        <link rel="stylesheet" type="text/css" href="main.css">
+    </head>
 
-        <label>Product Description:</label>
-        <span><?php echo $product_description; ?></span><br>
+    <body>
+        <main>
+            <h1>Product Discount Calculator</h1>
 
-        <label>List Price:</label>
-        <span><?php echo $list_price; ?></span><br>
+            <label>Product Description:</label>
+            <span><?php echo htmlspecialchars($product_description); ?></span><br>
 
-        <label>Standard Discount:</label>
-        <span><?php echo $discount_percent; ?></span><br>
+            <label>List Price:</label>
+            <span><?php echo htmlspecialchars($format_list_price); ?></span><br>
 
-        <label>Discount Amount:</label>
-        <span><?php echo ''; ?></span><br>
+            <label>Standard Discount:</label>
+            <span><?php echo htmlspecialchars($format_discount_percent); ?></span><br>
 
-        <label>Discount Price:</label>
-        <span><?php echo ''; ?></span><br>
-    </main>
-</body>
+            <label>Discount Amount:</label>
+            <span><?php echo $format_discount; ?></span><br>
+
+            <label>Discount Price:</label>
+            <span><?php echo $format_discount_price; ?></span><br>
+        </main>
+    </body>
 </html>
