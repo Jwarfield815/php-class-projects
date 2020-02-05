6240c06b335fde70b67e2e896dc3fca15aa0d73d
Jwarfield815 <creepathan@gmail.com>
2020-02-04 20:57:01 -0600

    finished excercise 2-2

--- exercise_2_2/display_results.php
+++ exercise_2_2/display_results.php
@@ -16,7 +16,9 @@
     } else if ( $interest_rate === FALSE )  {
         $error_message = 'Interest rate must be a valid number.'; 
     } else if ( $interest_rate <= 0 ) {
-        $error_message = 'Interest rate must be greater than zero.'; 
+        $error_message = 'Interest rate must be greater than zero.';
+    } else if ($interest_rate > 15) {
+        $error_message = 'Interest rate must be less than or equal to 15.';
     // validate years
     } else if ( $years === FALSE ) {
         $error_message = 'Years must be a valid whole number.';
@@ -67,5 +69,9 @@
         <label>Future Value:</label>
         <span><?php echo $future_value_f; ?></span><br>
     </main>
+
+    <footer>
+        <center>This calculation was done on <?php echo date('m/d/Y') ?></center>
+    </footer>
 </body>
 </html>
