d8431d24a4db25b7e57834d30fef9e83ac9084de
Jwarfield815 <creepathan@gmail.com>
2020-02-20 21:31:04 -0600

    added comments on code I edited

--- exercise_4_1/add_category.php
+++ exercise_4_1/add_category.php
@@ -1,17 +1,24 @@
 <?php
+    // get the new categories name from the request
     $category_name = filter_input(INPUT_POST, 'name');
 
+    // check if it's valid
     if (!$category_name) {
         $error = 'You must enter valid data for your new category name';
+        // displays error page with given error message
         include('error.php');
     } else {
+        // pulls the variable information from the database.php file
         require_once('database.php');
         $query = 'INSERT INTO categories (categoryName) VALUES (:category_name)';
+
+        // executes query string
         $statement = $db->prepare($query);
         $statement->bindValue(':category_name', $category_name);
         $statement-> execute();
         $statement->closeCursor();
 
+        // displays the page with the newly added items
         include('category_list.php');
     }
 ?>
