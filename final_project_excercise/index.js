2d345777f0de6261e08a9c8cc0785b2b8cf487ed
nate13nate <nate13nate@gmail.com>
2020-05-10 21:47:39 -0500

    Add Final Project Beginnings

---
+++ final_project_excercise/index.js
@@ -0,0 +1,219 @@
+/**
+ * This project allows the user to register multiple students for school.
+ * 
+ * Names: Nathanael Nading
+ *        Aaron Adams
+ *        Zakariya Sh-Adam
+ * Date:  April 15, 2020
+ */
+
+var entries = []; // holds the user's list of entries
+var form = document.getElementsByTagName("form")[0]; // holds the student registration form
+var successMessage = document.getElementById("submitSuccess"); // used to display a success message when the
+// user successfully submits an entry
+
+/**
+ * takes the fields from the form and ensures the requirements are met, specifically:
+ * the required fields (First_Name and Last_Name) are filled
+ * the First_Name, Last_Name, City, and State fields are less than 30 characters and use only letters
+ * the Mobile_Number field needs to be 10 numbers
+ * the Pin_Code field needs to be 6 numbers
+ * @param {Array} elements a list of the fields from the form
+ * @returns {String} an error message; will be the default value if there is not an error
+ */
+function ensureRequirementsAreMet(elements) {
+  // the message returned at the end of the function
+  // if it has changed by the end, then one of the required fields were not filled
+  var errorMessage = "Error: not all of the required fields were provided.";
+
+  elements.forEach(element => {
+    var { name, value } = element;
+    var smaller = 2 * 27;
+    var larger = smaller + 2 * 11;
+    console.log(smaller / larger);
+    console.log(smaller, larger);
+    var anotherOne = 100 - larger;
+    console.log((smaller + anotherOne) / (larger + anotherOne));
+    console.log(smaller / (larger + anotherOne));
+
+    // checks if it is one of the required fields and if the field has a value
+    // if the required field is not filled, then it adds to the error message
+    if (name === "First_Name" && value === "") {
+      errorMessage += "\nYou must include a first name.";
+    } else if (name === "Last_Name" && value === "") {
+      errorMessage += "\nYou must include a last name.";
+    } else { // if all the required fields are filled, then we do more, lesser priority checks
+      var numOfChars; // holds the number of alphabetical characters in the field
+      var numOfNums; // holds the number of numerical characters in the field
+      var lengthOfField = value.length || 0; // holds the length of the total field
+
+      // it tries to set numOfChars to the number of alphabetical characters in the field;
+      // however, if there are no alphabetical characters, then value.match()[0] will return null,
+      // making the .length reference cause an error. Since the error means there are no alphabetical characters,
+      // the numOfChars is set to 0
+      try {
+        var numOfChars = value.match(/^[A-Za-z]+$/)[0].length;
+      } catch (e) {
+        var numOfChars = 0;
+      }
+
+      // it tries to set numOfNums to the number of numerical characters in the field;
+      // however, if there are no numerical characters, then value.match()[0] will return null,
+      // making the .length reference cause an error. Since the error means there are no numeric characters,
+      // the numOfNums is set to 0
+      try {
+        var numOfNums = value.match(/^[0-9]+$/)[0].length;
+      } catch (e) {
+        var numOfNums = 0;
+      }
+
+
+      if (name === "Pin_Code" && lengthOfField !== 6 && lengthOfField !== 0) {
+        errorMessage = "Error: field Pin_Code must be 6 numbers.";
+      } else if ((name === "Mobile_Number" || name === "Pin_Code") && numOfNums < lengthOfField) {
+        errorMessage = `Error: field ${name} must use only numbers`;
+      } else if (name === "Mobile_Number" && lengthOfField !== 10 && lengthOfField !== 0) {
+        errorMessage = "Error: field Mobile Number must be 10 numbers.";
+      } else if ((name === "First_Name" || name === "Last_Name" || name === "City" || name === "State") && value.length > 30) {
+        errorMessage = `Error: field ${name} must have at least 30 characters.`;
+      } else if ((name === "First_Name" || name === "Last_Name" || name === "City" || name === "State") && numOfChars < lengthOfField) {
+        errorMessage = `Error: field ${name} must use only letters`;
+      }
+    }
+  });
+
+  return errorMessage;
+}
+
+/**
+ * removes the birthday fields from the entry and adds in a new, formatted birthday field
+ * @param {Array} entry a list of fields from the form
+ * @returns {Array} the list of fields from the form with formatted birthday fields (if applicable)
+ */
+function formatBirthday(entry) {
+  // the submitted fields which are a part of the birthday information
+  var dateInfo = entry.filter(field => {
+    return field.name === "Birthday_day" || field.name === "Birthday_Month" || field.name === "Birthday_Year";
+  });
+
+  // if there are birthday fields, then check which birthday fields there are
+  if (dateInfo.length > 0) {
+    // if all of the birthday fields are provided, then add all of the birthday information to the entry
+    if (dateInfo.length === 3) {
+      entry.push({ name: "Birthday", value: `${dateInfo[1].value} ${dateInfo[0].value}, ${dateInfo[2].value}` });
+    } else if (dateInfo[0].name === "Birthday_day" && dateInfo[1].name === "Birthday_Month") { // if the birth day and birth month are provided, then format them and add them to the entry
+      entry.push({ name: "Birthday", value: `${dateInfo[1].value} ${dateInfo[0].value}` });
+    } else if (dateInfo[0].name === "Birthday_Month" && dateInfo[1].name === "Birthday_Year") { // if the birth month and birth year are provided, then format them and add them to the entry
+      entry.push({ name: "Birthday", value: `${dateInfo[0].value}, ${dateInfo[1].value}` });
+    }
+  }
+
+  // remove the birthday fields from the entry
+  entry = entry.filter(field => {
+    return field.name !== "Birthday_day" && field.name !== "Birthday_Month" && field.name !== "Birthday_Year"
+  });
+
+  return entry;
+}
+
+// when the submit button is clicked, check the submitted form fields and ensure they include the required fields
+// if they do, then allow the form to submit; otherwise, prevent the form from submitting, display an error message,
+// and remove the successful submit message
+document.querySelector("input[type=submit]").addEventListener("click", (event) => {
+  var errorMessage = ensureRequirementsAreMet(Object.entries(event.srcElement.form.elements).map(element => element[1]));
+
+  if (errorMessage !== "Error: not all of the required fields were provided.") {
+    event.preventDefault();
+    window.alert(errorMessage);
+    successMessage.innerHTML = "";
+  }
+});
+
+// when the form has been subitted and the correct fields have been provided, remove the empty fields, format
+// the birthday field, add the entry to the entries list, clear the form, and display a successful submission message
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  var entry = Object.entries(event.srcElement.elements).filter((element) => {
+    // if the field has a value, it can continue along the chain of checks
+    if (element[1].value !== "" && element[1].value !== "-1" && element[1].name !== "") {
+      // the field "Gender" is a checkbox; since checkboxes always have a value, this checks if the checkbox is checked
+      return element[1].name !== "Gender" || element[1].checked;
+    }
+    // if the field does not have a value, then remove it from the entries
+    return false;
+  }).map(element => {
+    // returns an easier to use version of the field
+    return { name: element[1].name, value: element[1].value };
+  });
+
+  entry = formatBirthday(entry);
+
+  entries.push(entry);
+
+  document.querySelector("input[type=reset]").click();
+  successMessage.innerHTML = "Entry Submitted Successfully";
+});
+
+// when all of the entries are submitted, a final entry is added if there are any fields with a value,
+// the entries are checked to see if there are any, and the entries are displayed to the user
+document.getElementById("submitAll").addEventListener("click", (event) => {
+  // the errorMessage used for the ensureRequirementsAreMet function
+  var errorMessage = "Error: not all of the required fields were provided.";
+
+  event.preventDefault();
+
+  var entry = Object.entries(event.srcElement.form.elements).filter((element) => {
+    // if the field has a value, it can continue along the chain of checks
+    if (element[1].value !== "" && element[1].value !== "-1" && element[1].name !== "") {
+      // the field "Gender" is a checkbox; since checkboxes always have a value, this checks if the checkbox is checked
+      return element[1].name !== "Gender" || element[1].checked;
+    }
+    // if the field does not have a value, then remove it from the entries
+    return false;
+  }).map(element => {
+    // returns an easier to use version of the field
+    return { name: element[1].name, value: element[1].value };
+  });
+
+  entries.push(formatBirthday(entry));
+
+  // if the final entry has no completed fields, then the user just wants to submit the already created entries, so the last entry is deleted
+  if (entries[entries.length - 1].length === 0) {
+    entries.pop();
+  } else { // if the final entry has completed fields, ensure they include the required fields
+    errorMessage = ensureRequirementsAreMet(Object.entries(event.srcElement.form.elements).map(element => element[1]));
+  }
+
+  // if not all of the required fields were filled, display an error message
+  if (errorMessage !== "Error: not all of the required fields were provided.") {
+    entries.pop();
+    window.alert(errorMessage);
+    successMessage.innerHTML = "";
+  } else { // if the required fields were filled or there is no entry added, ensure there are entries in the entries array and display them
+    var entriesString = "";
+
+    // tries to fill the entries string with a formatted version of the submitted entries
+    try {
+      entries.forEach(entry => {
+        entry.forEach((field) => {
+          entriesString += `<div style='font-family:TimesNewRoman;color:grey;font-size:11pt;font-style:normal;font-weight:bold;text-align:center;background-color:lightskyblue;'>
+            ${field.name.replace("_", " ")}: ${field.value}
+            </div>`;
+        });
+
+        entriesString += "<br />";
+      });
+    } catch (error) { // if there are no entries (and entries.forEach will return an error), show an error message
+      entries.pop();
+      window.alert("Error: no entries have been submitted.");
+      successMessage.innerHTML = "";
+    } finally {
+      // if there was a list of entries, then display the entries string as HTML
+      if (entriesString !== "") {
+        document.write(entriesString);
+        document.getElementsByTagName('html')[0].style.backgroundColor = 'gray';
+      }
+    }
+  }
+});
